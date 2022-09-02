f1b56f46bed3bbf08bbf6461d7f1b5252030a4b1
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-09-02 15:43:58 -0400

    Update urls.py

--- graphs/urls.py
+++ graphs/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-# from . import views
+from . import views
 
 urlpatterns = [
-#     path("", views.index, name="index"),
-#     path("card/", views.card_overview, name="card-overview"),
-#     path("card/<str:cardName>", views.cards, name="card-info"),
-#     path("data-reception", views.data_reception, name="data-reception")
+    path("", views.index, name="index"),
+    path("card/", views.card_overview, name="card-overview"),
+    path("card/<str:cardName>", views.cards, name="card-info"),
+    path("data-reception", views.data_reception, name="data-reception")
 ]

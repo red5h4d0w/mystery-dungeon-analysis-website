58cc0e1c8619ee9e3349d679b85dd232931bd2aa
red5h4d0w <45214944+red5h4d0w@users.noreply.github.com>
2022-09-03 14:15:31 -0400

    add new url config

--- graphs/urls.py
+++ graphs/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("card/", views.card_overview, name="card-overview"),
     path("card/<str:cardName>", views.cards, name="card-info"),
-    path("data-reception", views.data_reception, name="data-reception")
+    path("data-reception", views.data_reception, name="data-reception"),
+    path("game/", views.game_overview, name="game-overview"),
+    path("game/<int:pk>", views.game, name="game-details")
 ]
